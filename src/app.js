7598c275a0b7e8e32b9ffcfee25e8113d2cdff84
andreasabater <155462711+andreasabater@users.noreply.github.com>
2025-09-24 19:19:04 +0000

    Cambios subidos andrea

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let sustantivos = ["tormenta eléctrica", "App del móvil", "el sistema"];
+  let acciones = ["causó un apagón", "se bloqueó", "falló"];
+  let evitar = ["no pudo acabar", "no debió fallar", "mojar"];
+  let cuando = ["ayer", "mientras estaba en el trabajo", "mientras programaba"];
+
+  let parte1 = Math.floor(Math.random() * sustantivos.length);
+  let parte2 = Math.floor(Math.random() * acciones.length);
+  let parte3 = Math.floor(Math.random() * evitar.length);
+  let parte4 = Math.floor(Math.random() * cuando.length);
+
+
+  document.querySelector("#excusas").innerHTML =
+  sustantivos[parte1] + acciones[parte2]+evitar[parte3]+cuando[parte4];
+       
 };
+
+
